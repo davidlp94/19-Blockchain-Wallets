093180a0a068ec4dfd3275e3dc941df89d1248bd
davidlp94 <96163075+davidlp94@users.noreply.github.com>
2022-05-11 18:59:42 -0400

    Updating application code.

--- fintech_finder.py
+++ fintech_finder.py
@@ -239,7 +239,7 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-wage = candidate_database[person][4] * hours
+wage = candidate_database[person][3] * hours
 st.sidebar.write(wage)
 
 # @TODO
@@ -271,7 +271,8 @@ if st.sidebar.button("Send Transaction"):
     # Call the `send_transaction` function and pass it 3 parameters:
     # Your `account`, the `candidate_address`, and the `wage` as parameters
     # Save the returned transaction hash as a variable named `transaction_hash`
-    # YOUR CODE HERE
+    send_transaction(account, candidate_address, wage)
+    transaction_hash = print(signed_tx)
 
     # Markdown for the transaction hash
     st.sidebar.markdown("#### Validated Transaction Hash")
